cf0b6051a8540baeda01d1ef61a698e8c48a9932
BlaKe00786 <sameershaikh403@gmail.com>
2020-09-29 13:03:47 +0530

    [Sameer]Refactor: added Line Equality check functionality.

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -8,7 +8,7 @@ namespace LineComparisonProblem
         {
             Console.WriteLine("Welcome to the Line Comparison Computation Program on Master branch");
             double x1_cordinate, x2_cordinate, y1_cordinate, y2_cordinate;
-            Console.WriteLine("Enter the co-ordinates of the end points of the line:");
+            Console.WriteLine("Enter the co-ordinates of the end points of the first line:");
             Console.WriteLine("X1:");
             x1_cordinate = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("X2");
@@ -17,8 +17,25 @@ namespace LineComparisonProblem
             y1_cordinate = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Y2");
             y2_cordinate = Convert.ToDouble(Console.ReadLine());
-            double lengthOfLine = Math.Sqrt(((x2_cordinate - x1_cordinate) * (x2_cordinate - x1_cordinate)) + ((y1_cordinate - y2_cordinate) * (y1_cordinate - y2_cordinate)));
-            Console.WriteLine(lengthOfLine.ToString("N3"));
+            double lengthOfLine1 = Math.Sqrt(((x2_cordinate - x1_cordinate) * (x2_cordinate - x1_cordinate)) + ((y1_cordinate - y2_cordinate) * (y1_cordinate - y2_cordinate)));
+            double a1_cordinate, a2_cordinate, b1_cordinate, b2_cordinate;
+            Console.WriteLine("Enter the co-ordinates of the end points of the second line:");
+            Console.WriteLine("A1:");
+            a1_cordinate = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("A2");
+            a2_cordinate = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("B1");
+            b1_cordinate = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("B2");
+            b2_cordinate = Convert.ToDouble(Console.ReadLine());
+            double lengthOfLine2 = Math.Sqrt(((a2_cordinate - a1_cordinate) * (a2_cordinate - a1_cordinate)) + ((b1_cordinate - b2_cordinate) * (b1_cordinate - b2_cordinate)));
+            if (lengthOfLine1 == lengthOfLine2)
+            {
+                Console.WriteLine("Both Lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both Lines are not equal");
+            }
         }
-    }
 }
