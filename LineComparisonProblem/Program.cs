f6da0bb37612243092238e13d6b208c5a3bdda28
BlaKe00786 <sameershaikh403@gmail.com>
2020-09-29 12:55:12 +0530

    [Sameer]Refactor:Added Line length calculation functionality

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -6,7 +6,19 @@ namespace LineComparisonProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome");
+            Console.WriteLine("Welcome to the Line Comparison Computation Program on Master branch");
+            double x1_cordinate, x2_cordinate, y1_cordinate, y2_cordinate;
+            Console.WriteLine("Enter the co-ordinates of the end points of the line:");
+            Console.WriteLine("X1:");
+            x1_cordinate = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("X2");
+            x2_cordinate = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Y1");
+            y1_cordinate = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Y2");
+            y2_cordinate = Convert.ToDouble(Console.ReadLine());
+            double lengthOfLine = Math.Sqrt(((x2_cordinate - x1_cordinate) * (x2_cordinate - x1_cordinate)) + ((y1_cordinate - y2_cordinate) * (y1_cordinate - y2_cordinate)));
+            Console.WriteLine(lengthOfLine.ToString("N3"));
         }
     }
 }
